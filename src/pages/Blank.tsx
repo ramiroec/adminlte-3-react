85cbb3b0b9ea92b668eb79dde9935a4e426b4280
Ramiro Estigarribia Canese <ramiroec@gmail.com>
2023-09-03 13:01:53 -0400

    Update Blank.tsx
    "The collapse and remove buttons are now fully functional."

--- src/pages/Blank.tsx
+++ src/pages/Blank.tsx
@@ -1,42 +1,51 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
-import {ContentHeader} from '@components';
+import React, { useState } from 'react';
+import { ContentHeader } from '@components';
+import { Collapse } from 'react-bootstrap';
 
 const Blank = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isRemoved, setIsRemoved] = useState(false);
   return (
     <div>
-      <ContentHeader title="Blank Page" />
+      <ContentHeader title="SubMenu Page" />
       <section className="content">
         <div className="container-fluid">
-          <div className="card">
-            <div className="card-header">
-              <h3 className="card-title">Title</h3>
-              <div className="card-tools">
-                <button
-                  type="button"
-                  className="btn btn-tool"
-                  data-widget="collapse"
-                  data-toggle="tooltip"
-                  title="Collapse"
-                >
-                  <i className="fa fa-minus" />
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-tool"
-                  data-widget="remove"
-                  data-toggle="tooltip"
-                  title="Remove"
-                >
-                  <i className="fa fa-times" />
-                </button>
+          {!isRemoved && (
+            <div className="card card-primary card-outline">
+              <div className="card-header">
+                <h3 className="card-title">Title</h3>
+                <div className="card-tools">
+                  <button
+                    onClick={() => setIsOpen(!isOpen)}
+                    type="button"
+                    className="btn btn-tool"
+                    data-widget="collapse"
+                    data-toggle="tooltip"
+                    title="Collapse"
+                  >
+                    <i className="fa fa-minus" />
+                  </button>
+                  <button
+                    onClick={() => setIsRemoved(true)}
+                    type="button"
+                    className="btn btn-tool"
+                    data-widget="remove"
+                    data-toggle="tooltip"
+                    title="Remove"
+                  >
+                    <i className="fa fa-times" />
+                  </button>
+                </div>
               </div>
+              <Collapse in={isOpen}>
+                <div className="card-body">
+                  Start creating your amazing application!
+                </div>
+              </Collapse>
+              <div className="card-footer">Footer</div>
             </div>
-            <div className="card-body">
-              Start creating your amazing application!
-            </div>
-            <div className="card-footer">Footer</div>
-          </div>
+          )}
         </div>
       </section>
     </div>
